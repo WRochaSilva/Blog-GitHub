67c457679c9f6d0d62c5e4d89e5e9fc8336614eb
Wesley <wesrocha26@gmail.com>
2023-11-14 08:20:48 -0300

    :seedling:Create Infos Profile

--- src/Components/Profile/index.tsx
+++ src/Components/Profile/index.tsx
@@ -1,10 +1,26 @@
-import { Avatar, ProfileContainer } from "./styles";
+import {
+  Avatar,
+  ProfileContainer,
+  ProfileDescription,
+  ProfileInfoContent,
+  ProfileName,
+  ProfileSocialMedia,
+} from "./styles";
 import imageAvatar from "../../assets/avatar.png";
 
 export const Profile = () => {
   return (
     <ProfileContainer>
       <Avatar src={imageAvatar} />
+      <ProfileInfoContent>
+        <ProfileName>Cameron Williamson</ProfileName>
+        <ProfileDescription>
+          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
+          viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat
+          pulvinar vel mass.
+        </ProfileDescription>
+        <ProfileSocialMedia>cameronwll</ProfileSocialMedia>
+      </ProfileInfoContent>
     </ProfileContainer>
   );
 };
--- src/Components/Profile/styles.ts
+++ src/Components/Profile/styles.ts
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const ProfileContainer = styled.div`
-  position: relative;
+  /* position: relative; */
+  display: flex;
   width: 864px;
   height: 212px;
   /* flex-shrink: 0; */
@@ -9,12 +10,32 @@ export const ProfileContainer = styled.div`
   background: ${(props) => props.theme["base-profile"]};
   box-shadow: 0px 2px 28px 0px rgba(0, 0, 0, 0.2);
   margin: -80px auto 0px;
+  padding: 30px;
 `;
 export const Avatar = styled.img`
-  position: absolute;
+  /* position: absolute; */
   width: 148px;
   height: 148px;
   /* flex-shrink: 0; */
   border-radius: 8px;
-  margin: 30px;
 `;
+export const ProfileInfoContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-left: 30px;
+`;
+export const ProfileName = styled.h1`
+  color: ${(props) => props.theme["base-title"]};
+  font-size: 24px;
+  font-weight: 700;
+`;
+export const ProfileDescription = styled.h2`
+  color: ${(props) => props.theme["base-text"]};
+  font-size: 16px;
+`;
+export const ProfileSocialMedia = styled.h3`
+  color: ${(props) => props.theme["base-subtitle"]};
+  font-size: 16px;
+`;
+export const LinkGitHub = styled.div``;
